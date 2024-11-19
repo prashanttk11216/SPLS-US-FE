b2172c81d826057af3e9428f087afcada51d7a6e
Mohammed Zishan Mudabbir Mohammed Akhtar Pash <mzmmapasha@virtualemployee.com>
2024-11-19 15:07:55 +0530

    Carrier Module created

--- src/pages/modules/Broker/Carrier/CarrierList/CarrierList.tsx
+++ src/pages/modules/Broker/Carrier/CarrierList/CarrierList.tsx
@@ -1,8 +1,171 @@
+import React, { useCallback, useEffect, useState } from "react";
+import PlusIcon from "../../../../../assets/icons/plus.svg";
+import Loading from "../../../../../components/common/Loading/Loading";
+import { UserRole } from "../../../../../enums/UserRole";
+import { toast } from "react-toastify";
+import {
+  deleteUser,
+  getUserById,
+  getUsers,
+} from "../../../../../services/user/userService";
+import Avatar from "../../../../../components/common/Avatar/Avatar";
+import { User } from "../../../../../types/User";
+import Table from "../../../../../components/common/Table/Table";
+import CreateOrEditCarrier from "../CreateOrEditCarrier/CreateOrEditCarrier";
+import useFetchData from "../../../../../hooks/useFetchData/useFetchData";
 
-const CarrierList = () => {
+const CarrierList: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [carrierToEdit, setCarrierToEdit] = useState<Partial<User> | null>(
+    null
+  );
+  const [customers, setCustomers] = useState<User[]>([]);
+
+  const {
+    fetchData: fetchCarriers,
+    fetchDataById: fetchCarrier,
+    deleteDataById: deleteCarrier,
+    loading,
+    error,
+  } = useFetchData<any>({
+    fetchDataService: getUsers,
+    fetchByIdService: getUserById,
+    deleteDataService: deleteUser,
+  });
+
+  //fetch Carrier data
+
+  const fetchCarrierData = useCallback(async () => {
+    try {
+      const result = await fetchCarriers(UserRole.CARRIER);
+      if (result.success) {
+        setCustomers(result.data);
+      } else {
+        toast.error(result.message || "Failed to fetch carrier.");
+      }
+    } catch (err) {
+      toast.error("Error fetching carrier data.");
+    }
+  }, [fetchCarriers]);
+
+  // Use a single fetch on initial render
+
+  useEffect(() => {
+    console.log("Log....");
+    fetchCarrierData();
+  }, [fetchCarrier]);
+
+  const columns = [
+    { key: "name", label: "Name", width: "40%" },
+    { key: "email", label: "Email" },
+    { key: "contact", label: "Contact" },
+    { key: "company", label: "Company" },
+    { key: "status", label: "Status" },
+    { key: "actions", label: "Actions", isAction: true },
+  ];
+
+  const handleActionClick = async (
+    action: string,
+    row: Record<string, any>
+  ) => {
+    if (action === "Edit") {
+      try {
+        const carrierData = await fetchCarrier(row._id);
+        openEditModal(carrierData.data);
+      } catch (err) {
+        toast.error("Failed to fetch customer details for editing.");
+      }
+    }
+    if (action === "Delete") {
+      try {
+        const result = await deleteCarrier(row._id);
+        if (result.success) {
+          toast.success(result.message);
+          fetchCarrierData();
+        }
+      } catch (err) {
+        toast.error("Failed to delete customer.");
+      }
+    }
+  };
+
+  const getRowData = () => {
+    return customers.map((carrier) => ({
+      _id: carrier._id,
+      name: (
+        <div className="d-flex align-items-center">
+          <div className="avatar_wrapper me-2">
+            <Avatar
+              avatarUrl={carrier.avatarUrl}
+              firstName={carrier.firstName}
+              lastName={carrier.lastName}
+              email={carrier.email}
+              size={35}
+            />
+          </div>
+          <div className="name">{`${carrier.firstName} ${carrier.lastName}`}</div>
+        </div>
+      ),
+      email: carrier.email,
+      contact: carrier.contactNumber || "N/A",
+      company: carrier.company || "N/A",
+      status: carrier.isActive ? "Active" : "Inactive",
+    }));
+  };
+
+
+  const openCreateModal = () => {
+    setIsEditing(false);
+    setCarrierToEdit(null);
+    setIsModalOpen(true);
+  };
+
+  const openEditModal = (carrierData: Partial<User>) => {
+    setIsEditing(true);
+    setCarrierToEdit(carrierData);
+    setIsModalOpen(true);
+  };
+
   return (
-    <div>Carrier List</div>
-  )
-}
+    <div className="customers-list-wrapper">
+      <h2 className="fw-bolder">Carrier List</h2>
+      <div className="d-flex align-items-center my-3">
+        <button
+          className="btn btn-accent d-flex align-items-center ms-auto"
+          type="button"
+          onClick={openCreateModal}
+        >
+          <img src={PlusIcon} height={16} width={16} className="me-2" />
+          Create
+        </button>
+      </div>
 
-export default CarrierList
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <div className="text-danger">{error}</div>
+      ) : (
+        <Table
+          columns={columns}
+          rows={getRowData()}
+          data={customers}
+          actions={["Edit", "Delete"]}
+          onActionClick={handleActionClick}
+        />
+      )}
+
+      <CreateOrEditCarrier
+        isModalOpen={isModalOpen}
+        setIsModalOpen={(value: boolean) => {
+          setIsModalOpen(value);
+          if (!value) fetchCarrierData(); // Refresh customers after modal close
+        }}
+        isEditing={isEditing}
+        carrierData={carrierToEdit}
+      />
+    </div>
+  );
+};
+
+export default CarrierList;
---
+++ src/pages/modules/Broker/Carrier/CreateOrEditCarrier/CreateOrEditCarrier.tsx
@@ -0,0 +1,276 @@
+import { FC, useEffect } from "react";
+import { User } from "../../../../../types/User";
+import { createUserForm } from "../../../../Auth/Signup/Signup";
+import useFetchData from "../../../../../hooks/useFetchData/useFetchData";
+import { createUser, editUser } from "../../../../../services/user/userService";
+import { UserRole } from "../../../../../enums/UserRole";
+import { toast } from "react-toastify";
+import Modal from "../../../../../components/common/Modal/Modal";
+import Loading from "../../../../../components/common/Loading/Loading";
+import Input from "../../../../../components/common/Input/Input";
+import { VALIDATION_MESSAGES } from "../../../../../constants/messages";
+import { REGEX_PATTERNS } from "../../../../../constants/patterns";
+import { useForm } from "react-hook-form";
+
+interface CreateOrEditCarrierProps {
+  isModalOpen: boolean; // Controls modal visibility
+  setIsModalOpen: (value: boolean) => void; // Setter for modal visibility
+  isEditing: boolean; // Indicates if editing an existing customer
+  carrierData?: Partial<User> | null; // Pre-filled data for editing
+}
+
+const CreateOrEditCarrier: FC<CreateOrEditCarrierProps> = ({
+  isModalOpen,
+  setIsModalOpen,
+  isEditing,
+  carrierData,
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+    reset,
+  } = useForm<createUserForm>({
+    mode: "onBlur",
+    defaultValues: carrierData || {}, // Pre-fill form when editing
+  });
+  const {
+    createData: createCarrier,
+    updateData: updateCarrier,
+    loading,
+    error,
+  } = useFetchData<any>({
+    createDataService: createUser,
+    updateDataService: editUser,
+  });
+
+  /**
+   * Handles form submission for creating or editing a carrier.
+   * @param data - Form data
+   */
+
+  const submit = async (data: createUserForm) => {
+    try {
+      let result;
+      if (isEditing && carrierData?._id) {
+        // Update carrier if editing
+        result = await updateCarrier(carrierData._id, data);
+      } else {
+        // Create carrier with role assigned
+        data.role = UserRole.CARRIER;
+        result = await createCarrier(data);
+      }
+
+      if (result.success) {
+        toast.success(
+          isEditing
+            ? "Customer updated successfully."
+            : "Customer created successfully."
+        );
+        setIsModalOpen(false);
+      } else {
+        throw new Error(result.message || "Action failed.");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      toast.error("An unexpected error occurred. Please try again.");
+    }
+  };
+
+  /**
+   * Validates if the confirmed password matches the entered password.
+   * @param value - Confirm password value
+   */
+  const validatePassword = (value: string) => {
+    return watch("password") === value || "Passwords do not match";
+  };
+
+  // Reset form state or pre-fill values when modal opens/closes
+  useEffect(() => {
+    if (isModalOpen) {
+      if (isEditing && carrierData) {
+        // Pre-fill form when editing
+        reset(carrierData);
+      } else {
+        // Clear form when creating
+        reset({
+          firstName: "",
+          lastName: "",
+          contactNumber: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+          company: "",
+        });
+      }
+    }
+  }, [isModalOpen, reset, isEditing, carrierData]);
+
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+      title={isEditing ? "Edit Carrier" : "Create Carrier"}
+      size="lg"
+      isCentered
+      backdropClose
+    >
+      {/* Show loader during API calls */}
+      {loading && <Loading />}
+
+      {/* Display error message if API fails */}
+      {error && (
+        <div className="alert alert-danger">
+          <strong>Error: </strong>
+          {error}
+        </div>
+      )}
+
+      {/* Form for creating/editing customer */}
+      <form onSubmit={handleSubmit(submit)}>
+        <div className="row mb-3">
+          {/* First Name */}
+          <div className="col-12 col-md-6">
+            <Input
+              label="First Name"
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="Enter First Name"
+              register={register}
+              errors={errors}
+              errorMessage={VALIDATION_MESSAGES.firstNameRequired}
+              required
+            />
+          </div>
+
+          {/* Last Name */}
+          <div className="col-12 col-md-6">
+            <Input
+              label="Last Name"
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="Enter Last Name"
+              register={register}
+              errors={errors}
+              errorMessage={VALIDATION_MESSAGES.lastNameRequired}
+              required
+            />
+          </div>
+
+          {/* Contact Number */}
+          <div className="col-12 col-md-6">
+            <Input
+              label="Contact Number"
+              type="text"
+              id="contactNumber"
+              name="contactNumber"
+              placeholder="Enter Contact Number"
+              register={register}
+              errors={errors}
+              errorMessage={VALIDATION_MESSAGES.contactNumberRequired}
+              required
+            />
+          </div>
+
+          {/* Email */}
+          <div className="col-12 col-md-6">
+            <Input
+              label="Email"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@example.com"
+              register={register}
+              errors={errors}
+              validationMessages={{
+                required: VALIDATION_MESSAGES.emailRequired,
+                pattern: VALIDATION_MESSAGES.emailInvalid,
+              }}
+              pattern={REGEX_PATTERNS.email}
+              required
+              disabled={isEditing} // Disable email during editing
+            />
+          </div>
+
+          {/* Password (only for creating) */}
+          {!isEditing && (
+            <>
+              <div className="col-12 col-md-6">
+                <Input
+                  label="Password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Enter Password"
+                  register={register}
+                  errors={errors}
+                  validationMessages={{
+                    required: VALIDATION_MESSAGES.passwordRequired,
+                    pattern: VALIDATION_MESSAGES.passwordPattern,
+                  }}
+                  pattern={REGEX_PATTERNS.password}
+                  required
+                />
+              </div>
+
+              <div className="col-12 col-md-6">
+                <Input
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  register={register}
+                  errors={errors}
+                  validationMessages={{
+                    required: VALIDATION_MESSAGES.confirmPasswordRequired,
+                  }}
+                  validateFun={validatePassword}
+                  required
+                />
+              </div>
+            </>
+          )}
+
+          {/* Company Name */}
+          <div className="col-12">
+            <Input
+              label="Company Name"
+              type="text"
+              id="company"
+              name="company"
+              placeholder="Enter Company Name"
+              register={register}
+              errors={errors}
+            />
+          </div>
+        </div>
+
+        {/* Action Buttons */}
+        <div className="row">
+          <div className="col-12 mb-1 text-end">
+            <button
+              className="btn btn-secondary me-3"
+              type="button"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Close
+            </button>
+            <button
+              className="btn btn-accent"
+              type="submit"
+              disabled={!isValid || loading}
+            >
+              {isEditing ? "Update" : "Create"}
+            </button>
+          </div>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default CreateOrEditCarrier;
