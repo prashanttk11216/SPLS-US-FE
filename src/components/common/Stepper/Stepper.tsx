7e1cb46f4e706e3bc5e84e8befc2f6b9012306f1
Ashik Kumar Patel <akpatel@virtualemployee.com>
2024-11-21 17:45:25 +0530

    feat: add stepper in customer form

--- src/components/common/Stepper/Stepper.tsx
+++ src/components/common/Stepper/Stepper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Stepper.scss';
 
 /**
@@ -7,65 +7,76 @@ import './Stepper.scss';
 export type Step = {
   label: string; // The label of the step
   content: React.ReactNode; // The content to display for the step
+  fields?: any[]
 };
 
 /**
  * Props for the Stepper component.
  */
 export type StepperProps = {
-    steps: Step[]; // Array of steps
-    orientation?: 'horizontal' | 'vertical'; // Stepper orientation
-    linear?: boolean; // Whether progression is linear
-    onComplete?: () => void; // Callback for when the stepper is completed
-    activeStep: number; // Externally controlled active step
-    setActiveStep: React.Dispatch<React.SetStateAction<number>>; // External setter for active step
-    completedSteps: number[];
+  steps: Step[]; // Array of steps
+  orientation?: 'horizontal' | 'vertical'; // Stepper orientation
+  linear?: boolean; // Whether progression is linear
+  onComplete?: () => void; // Callback for when the stepper is completed
+  activeStep: number; // Externally controlled active step
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>; // External setter for active step
+  completedSteps: number[];
 };
   
 
 const Stepper: React.FC<StepperProps> = ({
-    steps,
-    orientation = 'horizontal',
-    linear = true,
-    onComplete,
-    activeStep,
-    setActiveStep,
-    completedSteps
-  }) => {
-  
-    const isStepComplete = (stepIndex: number) =>
-      completedSteps.includes(stepIndex);
-  
-    const handleStepClick = (stepIndex: number) => {
-      if (!linear || isStepComplete(stepIndex) || stepIndex === activeStep) {
-        setActiveStep(stepIndex);
-      }
-    };
-  
-    return (
-      <div className={`stepper stepper-${orientation}`}>
-        {/* Stepper Header */}
-        <div className="stepper-header">
-          {steps.map((step, index) => (
-            <div
-              key={index}
-              className={`stepper-step ${
-                activeStep === index ? 'active' : ''
-              } ${isStepComplete(index) ? 'completed' : ''}`}
-              onClick={() => handleStepClick(index)}
-            >
-              <div className="stepper-indicator">{index + 1}</div>
-              <div className="stepper-label">{step.label}</div>
-              {index < steps.length - 1 && (
-                <div className="stepper-connector"></div>
-              )}
-            </div>
-          ))}
-        </div>
-        <div className="stepper-content">{steps[activeStep].content}</div>
-      </div>
-    );
+  steps,
+  orientation = 'horizontal',
+  linear = true,
+  activeStep,
+  setActiveStep,
+  completedSteps,
+}) => {
+  const isStepComplete = (stepIndex: number) =>
+    completedSteps.includes(stepIndex);
+
+  const handleStepClick = (stepIndex: number) => {
+    if (!linear || isStepComplete(stepIndex) || stepIndex === activeStep) {
+      setActiveStep(stepIndex);
+    }
   };
-  
+
+  return (
+    <div className={`stepper stepper-${orientation}`}>
+      {/* Stepper Header */}
+      <div className="stepper-header">
+        {steps.map((step, index) => (
+          <div
+            key={index}
+            className={`stepper-step ${
+              activeStep === index ? 'active' : ''
+            } ${isStepComplete(index) ? 'completed' : ''}`}
+            onClick={() => handleStepClick(index)}
+          >
+            <div className="stepper-indicator">{index + 1}</div>
+            <div className="stepper-label">{step.label}</div>
+            {/* {index < steps.length - 1 && (
+              <div className="stepper-connector"></div>
+            )} */}
+          </div>
+        ))}
+      </div>
+
+      {/* Stepper Content */}
+      <div className="stepper-content">
+        {steps.map((step, index) => (
+          <div
+            key={index}
+            className={`step-content ${
+              activeStep === index ? 'visible' : 'hidden'
+            }`}
+          >
+            {step.content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Stepper;
--- src/pages/modules/Broker/Customer/CreateOrEditCustomer/CreateOrEditCustomer.tsx
+++ src/pages/modules/Broker/Customer/CreateOrEditCustomer/CreateOrEditCustomer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import Modal from "../../../../../components/common/Modal/Modal";
 import { VALIDATION_MESSAGES } from "../../../../../constants/messages";
 import Input from "../../../../../components/common/Input/Input";
@@ -15,6 +15,9 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../../../store/store";
 import { PhoneInput } from "react-international-phone";
 import { validatePhoneNumber } from "../../../../../utils/phoneValidate";
+import Stepper, {
+  Step,
+} from "../../../../../components/common/Stepper/Stepper";
 
 interface CreateOrEditCustomerProps {
   isModalOpen: boolean; // Controls modal visibility
@@ -29,9 +32,11 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
   setIsModalOpen,
   isEditing,
   customerData,
-  closeModal
+  closeModal,
 }) => {
   const user = useSelector((state: RootState) => state.user);
+  const [activeStep, setActiveStep] = useState(0); // Tracks current step
+  const [completedSteps, setCompletedSteps] = useState<number[]>([]);
   const {
     register,
     handleSubmit,
@@ -39,6 +44,7 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
     formState: { errors, isValid },
     watch,
     reset,
+    trigger,
   } = useForm<createUserForm>({
     mode: "onBlur",
     defaultValues: customerData || {}, // Pre-fill form when editing
@@ -73,9 +79,12 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
 
       if (result.success) {
         toast.success(
-          isEditing ? "Customer updated successfully." : "Customer created successfully."
+          isEditing
+            ? "Customer updated successfully."
+            : "Customer created successfully."
         );
         setIsModalOpen(false);
+        resetSteps();
       } else {
         throw new Error(result.message || "Action failed.");
       }
@@ -104,7 +113,7 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
         reset({
           firstName: "",
           lastName: "",
-          contactNumber: "",
+          primaryNumber: "",
           email: "",
           password: "",
           confirmPassword: "",
@@ -113,11 +122,195 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
       }
     }
   }, [isModalOpen, reset, isEditing, customerData]);
+  
+
+  const nextStep = async () => {
+    const stepFields = steps[activeStep].fields || [];
+    const isValidStep = await trigger(stepFields);
+    if (isValidStep) {
+        setCompletedSteps((prev) => [...prev, activeStep]);
+        setActiveStep((prev) => prev + 1);
+    } else {
+        toast.error('Please correct the errors before proceeding.');
+    }
+};
+
+
+  const prevStep = () => {
+    // setCompletedSteps((prev) => prev.filter((step) => step !== activeStep - 1)); // Optionally remove completion status for the previous step
+    setActiveStep((prev) => Math.max(0, prev - 1)); // Safeguard against going below step 0
+  };
+
+  const resetSteps = () => {
+    setActiveStep(0);
+    setCompletedSteps([]); // Clear all completed steps
+  };
+
+  const steps: Step[] = [
+    {
+      label: "Basic Details",
+      content: (
+        <>
+          <div className="row">
+            {/* First Name */}
+            <div className="col-12 col-md-6">
+              <Input
+                label="First Name"
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="Enter First Name"
+                register={register}
+                errors={errors}
+                errorMessage={VALIDATION_MESSAGES.firstNameRequired}
+                required
+              />
+            </div>
+
+            {/* Last Name */}
+            <div className="col-12 col-md-6">
+              <Input
+                label="Last Name"
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="Enter Last Name"
+                register={register}
+                errors={errors}
+                errorMessage={VALIDATION_MESSAGES.lastNameRequired}
+                required
+              />
+            </div>
+
+            {/* Primary Number */}
+            <div className="col-12 col-md-6">
+              <label className="form-label text-dark-blue">
+                Primary Number{"*"}
+              </label>
+              <Controller
+                name="primaryNumber"
+                control={control}
+                rules={{
+                  required: VALIDATION_MESSAGES.primaryNumberRequired,
+                  validate: validatePhoneNumber,
+                }}
+                render={({ field }) => (
+                  <>
+                    <PhoneInput
+                      {...field}
+                      defaultCountry="us"
+                      required
+                      className={errors.primaryNumber ? "phone-is-invalid" : ""}
+                      inputClassName={`w-100 phone-input form-control ${
+                        errors.primaryNumber ? "is-invalid" : ""
+                      }`}
+                      onChange={(phone) => field.onChange(phone)}
+                    />
+                    {errors.primaryNumber && (
+                      <div className="text-danger">
+                        {errors.primaryNumber.message}
+                      </div>
+                    )}
+                  </>
+                )}
+              />
+            </div>
+
+            {/* Email */}
+            <div className="col-12 col-md-6">
+              <Input
+                label="Email"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="name@example.com"
+                register={register}
+                errors={errors}
+                validationMessages={{
+                  required: VALIDATION_MESSAGES.emailRequired,
+                  pattern: VALIDATION_MESSAGES.emailInvalid,
+                }}
+                pattern={REGEX_PATTERNS.email}
+                required
+                disabled={isEditing} // Disable email during editing
+              />
+            </div>
+          </div>
+        </>
+      ),
+      fields: ['firstName', 'lastName', 'primaryNumber', 'email']
+    },
+    {
+      label: "Security",
+      content: (
+        <>
+          <div className="row">
+            {/* Password (only for creating) */}
+            {!isEditing && (
+              <>
+                <div className="col-12 col-md-6">
+                  <Input
+                    label="Password"
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    register={register}
+                    errors={errors}
+                    validationMessages={{
+                      required: VALIDATION_MESSAGES.passwordRequired,
+                      pattern: VALIDATION_MESSAGES.passwordPattern,
+                    }}
+                    pattern={REGEX_PATTERNS.password}
+                    required
+                  />
+                </div>
+
+                <div className="col-12 col-md-6">
+                  <Input
+                    label="Confirm Password"
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    register={register}
+                    errors={errors}
+                    validationMessages={{
+                      required: VALIDATION_MESSAGES.confirmPasswordRequired,
+                    }}
+                    validateFun={validatePassword}
+                    required
+                  />
+                </div>
+              </>
+            )}
+
+            {/* Company Name */}
+            <div className="col-12">
+              <Input
+                label="Company Name"
+                type="text"
+                id="company"
+                name="company"
+                placeholder="Enter Company Name"
+                register={register}
+                errors={errors}
+              />
+            </div>
+          </div>
+        </>
+      ),
+      fields: ['password', 'confirmPassword']
+    }
+  ];
 
   return (
     <Modal
       isOpen={isModalOpen}
-      onClose={closeModal}
+      onClose={()=> {
+        resetSteps();
+        closeModal();
+      }}
       title={isEditing ? "Edit Customer" : "Create Customer"}
       size="lg"
       isCentered
@@ -129,172 +322,51 @@ const CreateOrEditCustomer: FC<CreateOrEditCustomerProps> = ({
       {/* Display error message if API fails */}
       {error && (
         <div className="alert alert-danger">
-          <strong>Error: </strong>{error}
+          <strong>Error: </strong>
+          {error}
         </div>
       )}
 
       {/* Form for creating/editing customer */}
-      <form onSubmit={handleSubmit(submit)}>
-        <div className="row mb-3">
-          {/* First Name */}
-          <div className="col-12 col-md-6">
-            <Input
-              label="First Name"
-              type="text"
-              id="firstName"
-              name="firstName"
-              placeholder="Enter First Name"
-              register={register}
-              errors={errors}
-              errorMessage={VALIDATION_MESSAGES.firstNameRequired}
-              required
-            />
-          </div>
-
-          {/* Last Name */}
-          <div className="col-12 col-md-6">
-            <Input
-              label="Last Name"
-              type="text"
-              id="lastName"
-              name="lastName"
-              placeholder="Enter Last Name"
-              register={register}
-              errors={errors}
-              errorMessage={VALIDATION_MESSAGES.lastNameRequired}
-              required
-            />
-          </div>
-
-          {/* Contact Number */}
-
-          <div className="col-12 col-md-6">
-            <label className="form-label text-dark-blue">
-              Contact Number{"*"}
-            </label>
-            <Controller
-              name="contactNumber"
-              control={control}
-              rules={{
-                required: VALIDATION_MESSAGES.contactNumberRequired,
-                validate: validatePhoneNumber,
-              }}
-              render={({ field }) => (
-                <>
-                  <PhoneInput
-                    {...field}
-                    defaultCountry="us"
-                    required
-                    className={errors.contactNumber ? "phone-is-invalid" : ""}
-                    inputClassName={`w-100 phone-input form-control ${
-                      errors.contactNumber ? "is-invalid" : ""
-                    }`}
-                    onChange={(phone) => field.onChange(phone)}
-                  />
-                  {errors.contactNumber && (
-                    <div className="text-danger">
-                      {errors.contactNumber.message}
-                    </div>
-                  )}
-                </>
-              )}
-            />
-          </div>
-
-          {/* Email */}
-          <div className="col-12 col-md-6">
-            <Input
-              label="Email"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="name@example.com"
-              register={register}
-              errors={errors}
-              validationMessages={{
-                required: VALIDATION_MESSAGES.emailRequired,
-                pattern: VALIDATION_MESSAGES.emailInvalid,
-              }}
-              pattern={REGEX_PATTERNS.email}
-              required
-              disabled={isEditing} // Disable email during editing
-            />
-          </div>
-
-          {/* Password (only for creating) */}
-          {!isEditing && (
-            <>
-              <div className="col-12 col-md-6">
-                <Input
-                  label="Password"
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Enter Password"
-                  register={register}
-                  errors={errors}
-                  validationMessages={{
-                    required: VALIDATION_MESSAGES.passwordRequired,
-                    pattern: VALIDATION_MESSAGES.passwordPattern,
-                  }}
-                  pattern={REGEX_PATTERNS.password}
-                  required
-                />
-              </div>
-
-              <div className="col-12 col-md-6">
-                <Input
-                  label="Confirm Password"
-                  type="password"
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  placeholder="Confirm Password"
-                  register={register}
-                  errors={errors}
-                  validationMessages={{
-                    required: VALIDATION_MESSAGES.confirmPasswordRequired,
-                  }}
-                  validateFun={validatePassword}
-                  required
-                />
-              </div>
-            </>
-          )}
-
-          {/* Company Name */}
-          <div className="col-12">
-            <Input
-              label="Company Name"
-              type="text"
-              id="company"
-              name="company"
-              placeholder="Enter Company Name"
-              register={register}
-              errors={errors}
-            />
-          </div>
-        </div>
-
-        {/* Action Buttons */}
+        <Stepper
+          steps={steps}
+          activeStep={activeStep}
+          setActiveStep={setActiveStep}
+          completedSteps={completedSteps}
+          linear
+        />
         <div className="row">
-          <div className="col-12 mb-1 text-end">
+          <div className="col-6 text-start">
             <button
-              className="btn btn-secondary me-3"
+              className="btn btn-secondary"
               type="button"
-              onClick={() => setIsModalOpen(false)}
+              onClick={prevStep}
+              disabled={activeStep === 0}
             >
-              Close
-            </button>
-            <button
-              className="btn btn-accent"
-              type="submit"
-              disabled={!isValid || loading}
-            >
-              {isEditing ? "Update" : "Create"}
+              Previous
             </button>
           </div>
+          <div className="col-6 text-end">
+            {activeStep < steps.length - 1 ? (
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={nextStep}
+              >
+                Next
+              </button>
+            ) : (
+              <button
+                className="btn btn-accent"
+                type="submit"
+                disabled={!isValid || loading}
+                onClick={handleSubmit(submit)}
+              >
+                {isEditing ? "Update" : "Create"}
+              </button>
+            )}
+          </div>
         </div>
-      </form>
     </Modal>
   );
 };
