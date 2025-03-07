9587482047e17d5246c9a8e34088c8d0ba7aba71
Ashik Kumar Patel <akpatel@virtualemployee.com>
2025-03-07 17:38:29 +0530

    Fix: remove math functions

--- src/pages/modules/Carrier/dashboard/CarrierDashboard.tsx
+++ src/pages/modules/Carrier/dashboard/CarrierDashboard.tsx
@@ -88,7 +88,7 @@ const CarrierDashboard: React.FC = () => {
         <h3>Stats</h3>
         <div className="dashboard">
           <DashboardTile
-            key={Math.random()}
+            key={"Stats" + 1}
             title={"Active Loads"}
             value={stats.activeLoads}
             suffix="loads"
@@ -96,7 +96,7 @@ const CarrierDashboard: React.FC = () => {
             handleClick={handleTruckLoadNavigation}
           />
           <DashboardTile
-            key={Math.random()}
+            key={"Stats" + 2}
             title={"Completed Loads"}
             value={stats.completedLoads}
             suffix="loads"
@@ -104,7 +104,7 @@ const CarrierDashboard: React.FC = () => {
             handleClick={handleTruckLoadNavigation}
           />
           <DashboardTile
-            key={Math.random()}
+            key={"Stats" + 3}
             title={"Total Trucks"}
             value={stats.totalTrucks}
             suffix="loads"
@@ -112,7 +112,7 @@ const CarrierDashboard: React.FC = () => {
             handleClick={handleTruckLoadNavigation}
           />
           <DashboardTile
-            key={Math.random()}
+            key={"Stats" + 4}
             title={"Total Earnings"}
             value={`$${formatNumber(stats.totalEarnings)}`}
             color="#4caf50"
